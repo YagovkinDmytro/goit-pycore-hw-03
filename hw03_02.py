872e09cca68f0840f5a897de480eb6a6192bc432
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2025-02-26 15:28:33 +0100

    fixed  hw03_02 and hw03_04

--- hw03_02.py
+++ hw03_02.py
@@ -6,7 +6,7 @@ def get_numbers_ticket(min, max, quantity):
         if (min >= 1 and max <= 1000) and (min <= quantity <= max):
             for i in range(min, max + 1):
                 number_list.append(i)
-        ticket_numbers = (random.choices(number_list, k=quantity))
+        ticket_numbers = (random.sample(number_list, k=quantity))
         ticket_numbers.sort()
         print(f" Your ticket numbers: {ticket_numbers}")
     except:
@@ -14,5 +14,5 @@ def get_numbers_ticket(min, max, quantity):
         print(number_list)
 
 
-get_numbers_ticket(1, 100, 6)
+get_numbers_ticket(1, 6, 6)
 
